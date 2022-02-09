1dde37e2b6cca6b8a1efa7c17a41d74dac403cca
Johad Toledo <johadtledo@gmail.com>
2022-02-09 10:21:54 +0100

    Building the differents pages

--- public/tobuy.js
+++ public/tobuy.js
@@ -1,4 +1,21 @@
+let homeEmpty = document.getElementById('home-empty')
 let ulContainer = document.getElementById('ul-container')
+let containerList = document.getElementById('container-list')
+
+
+
+
+
+
+let btnAdd = document.getElementById('btn-add').addEventListener('click', function() {
+      homeEmpty.style.display = 'none';
+      containerList.style.display = 'block';
+})
+
+
+
+
+
 
 let addBtn = document
   .getElementById("add")
