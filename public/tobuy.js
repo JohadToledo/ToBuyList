a22ec4007e3afe718e39839b3fdbad7192ab3d96
Johad Toledo <johadtledo@gmail.com>
2022-02-10 21:59:19 +0100

    add some img as icons

--- public/tobuy.js
+++ public/tobuy.js
@@ -26,9 +26,9 @@ function back() {
 function btnDetail(title, type, description) {
   let newItem = `
                     <div class="card-detail">
-                    <span>${type}</span>
-                      <h5>${title}</h5>
-                      <p>${description}<p/>
+                    <img src="${type}" class="icon-img" >
+                      <h5 class='h5-title'>${title}</h5>
+                      <p class='p-description'>${description}<p/>
                       <button class="btn btn-close" onclick='back()'><span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
                       <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
                     </svg></span></button>
@@ -37,9 +37,9 @@ function btnDetail(title, type, description) {
 
   prod.innerHTML = newItem;
 console.log(type)
-if(type == 'Sumermarket') {
+if(type.value == 'Sumermarket') {
   document.querySelector('.card-detail').classList.add('background-Sumermarket')
-}else if(type === '👘'){
+}else if(type === './img/Chothing.png'){
   document.querySelector('.card-detail').classList.add('background-Clothing')
 }else if(type == 'Technology'){
   document.querySelector('.card-detail').classList.add('background-Technology')
@@ -68,8 +68,8 @@ let addBtnForm = document
     if(title != '' && type != '' && description != ''){
       let item = `
       <div class="card">
-      <span>${type}</span>
-      <h5>${title}</h5>
+      <img src="${type}" class="icon-img" >
+      <h5 class='h5-title'>${title}</h5>
       <a class=btn-list onClick="btnDetail('${title}', '${type}', '${description}')"><span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
       <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
     </svg></span></a>
