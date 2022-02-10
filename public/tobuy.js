5b7ce21e486a7b6a616848cf076d6d1b928d604c
Johad Toledo <johadtledo@gmail.com>
2022-02-10 02:07:35 +0100

    Only need some feedback

--- public/tobuy.js
+++ public/tobuy.js
@@ -53,19 +53,23 @@ let addBtnForm = document
     let title = document.getElementById("formTitle").value;
     let type = document.getElementById("formType").value;
     let description = document.getElementById("formDescription").value;
-    let item = `
-    <div class="card">
-    <span>${type}</span>
-    <h5>${title}</h5>
-    <a class=btn-list onClick="btnDetail('${title}', '${type}', '${description}')"><span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-    <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
-  </svg></span></a>
-    </div>`;
-    ulContainer.innerHTML += item;
-
-    sectionPopupForm.style.display = "none";
-    sectionHome.style.display = "none";
-    sectionList.style.display = "flex";
-
-    toBuyForm.reset();
+    if(title != '' && type != '' && description != ''){
+      let item = `
+      <div class="card">
+      <span>${type}</span>
+      <h5>${title}</h5>
+      <a class=btn-list onClick="btnDetail('${title}', '${type}', '${description}')"><span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+    </svg></span></a>
+      </div>`;
+      ulContainer.innerHTML += item;
+  
+      sectionPopupForm.style.display = "none";
+      sectionHome.style.display = "none";
+      sectionList.style.display = "flex";
+  
+      toBuyForm.reset();
+    }else{
+      document.getElementById('message').innerHTML = "Complete the fields firts :)"
+    }
   });
